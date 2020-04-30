dae4c4e103c5f8ea83298701eb754bbdda1b9537
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-04-30 14:57:06 +0100

    Types: Document HttpRequestPriority

--- types.d.ts
+++ types.d.ts
@@ -5,8 +5,18 @@
     SPDX-License-Identifier: MIT
 */
 
+/**
+ * Defines the priority of a given request in the queue.
+ *
+ * @param First The request will be placed at the front of the prioritary queue.
+ * @param Prioritary The request will be placed at the back of the prioritary queue.
+ * @param Normal The request will be placed at the back of the regular queue.
+ */
 interface HttpRequestPriority {
+    /**The request will be placed at the front of the prioritary queue. */
     First: number,
+    /**The request will be placed at the back of the prioritary queue. */
     Prioritary: number,
+    /**The request will be placed at the back of the regular queue. */
     Normal: number
 }
