c12e930e411c3eb23182b8197552d5a09b959ce9
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-04-30 20:13:34 +0100

    Define HttpRequest and HttpResponse interfaces

--- types.d.ts
+++ types.d.ts
@@ -12,11 +12,105 @@
  * @param Prioritary The request will be placed at the back of the prioritary queue.
  * @param Normal The request will be placed at the back of the regular queue.
  */
+
+type StringDictionary = { [k: string]: keyof string };
+
 interface HttpRequestPriority {
     /**The request will be placed at the front of the prioritary queue. */
-    First: number,
+    First: number;
     /**The request will be placed at the back of the prioritary queue. */
-    Prioritary: number,
+    Prioritary: number;
     /**The request will be placed at the back of the regular queue. */
-    Normal: number
+    Normal: number;
+}
+
+/**
+ * Defines an Http request.
+ */
+interface HttpRequest {
+    /**
+     * The computed url to send the request to
+     */
+    readonly Url: string;
+
+    /**
+     * Sends the request to the specified Url.
+     *
+     * @returns A promise to a HttpResponse that is resolved when it is available.
+     */
+    Send(): Promise<HttpResponse>;
+
+    /**
+     * @yields
+     *
+     * @returns The server's response to the request.
+     */
+    AwaitSend(): HttpResponse;
+}
+
+interface HttpRequestConstructor {
+    /**
+     * @constructor Creates an HttpRequest
+     *
+     * @param Url The url endpoint the request is being sent to.
+     * @param Method A string containing the method/verb being used in the request.
+     * @param Body The body of the request. Only applicable if you're going to send data (POST, PUT, etc.)
+     * @param Query Url query options (which are then appended to the url)
+     * @param Headers Additional headers to be included in the request
+     *
+     * @example
+     * let request = new HttpRequest("https://example.org", "GET", {
+     *     isCool: true,
+     *     qwerty: "keyboard",
+     *     from: "roblox"
+     * }, )
+     */
+    new (
+        Url: string,
+        Method: string,
+        Query?: { [k: string]: keyof string | number | boolean } | undefined,
+        Body?: string | undefined,
+        Headers?: StringDictionary | undefined,
+    ): HttpRequest;
+}
+
+interface HttpResponse {
+    /**
+     * Whether the connection to the remote server was successful. This is related to HttpService itself.
+     * This field can carry a value of false in the following conditions:
+     *
+     * - HttpService is disabled;
+     * - The remote server is down or refusing to connect;
+     * - Trust issues with the TLS certificates;
+     * - Other issues not completely related to the protocol itself.
+     *
+     * If this value is false, all other values are undefined.
+     */
+    readonly ConnectionSuccessful: boolean;
+
+    /**
+     * Whether the request to the server was successful. This is directly tied to the request itself.
+     * It will be true if the status code is within the range of 200-299, false otherwise.
+     */
+    readonly RequestSuccessful: boolean | undefined;
+
+    /**
+     * The status code returned by the remote server.
+     */
+    readonly StatusCode: number;
+
+    /**
+     * An human-readable string representation of the status code returned by the remote server.
+     */
+    readonly StatusMessage: string;
+
+    /**
+     * A dictionary containing the response headers returned by the remote server.
+     */
+    readonly Headers: StringDictionary;
+
+    /**
+     * The data returned by the server.
+     */
+    readonly Body: string;
 }
