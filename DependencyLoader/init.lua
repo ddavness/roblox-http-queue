58eed3e8a773d263dba769b6bf8bf710b7116a88
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-05-05 14:18:55 +0100

    Reconfigure dependency loader when loading from the Roblox-TS library

--- DependencyLoader/init.lua
+++ DependencyLoader/init.lua
@@ -18,7 +18,19 @@ local TS = _G[script.Parent]
 if TS then
     warn("Loaded via TS")
     dependencies.Promise = TS.Promise
-    dependencies.t = TS.import(script, TS.getModule(script, "t"))
+
+    -- TS's promise interface is different so we have to come up with an adapter
+    function dependencies.Promise.async(callback)
+        local promise = dependencies.Promise.new(function(resolve, reject, onCancel)
+            dependencies.Promise.spawn(function()
+                callback(resolve, reject, onCancel)
+            end)
+        end)
+
+        return promise
+    end
+
+    dependencies.t = TS.import(script.Parent, TS.getModule(script.Parent, "t").lib.ts)
 else
     warn("Loading locally")
     -- Load dependencies locally
