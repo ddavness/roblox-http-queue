52127c93f7b8989878b0bca52a33562eab4be19a
David Duque <david@duqued.net>
2021-03-14 00:26:29 +0000

    Update submodules
    - t to v2.1.1
    - Promise to v3.1.0

--- DependencyLoader/init.lua
+++ DependencyLoader/init.lua
@@ -16,27 +16,13 @@ local TS = _G[script.Parent]
 
 if TS then
     -- Module was loaded via Roblox-TS
-    --[[dependencies.Promise = TS.Promise
-
-    -- TS's promise interface is different so we have to come up with an adapter
-    function dependencies.Promise.async(callback)
-        local promise = dependencies.Promise.new(function(resolve, reject, onCancel)
-            dependencies.Promise.spawn(function()
-                callback(resolve, reject, onCancel)
-            end)
-        end)
-
-        return promise
-    end]]
-
+    dependencies.Promise = TS.Promise
     dependencies.t = TS.import(script.Parent, TS.getModule(script.Parent, "t").lib.ts)
 else
     -- Load dependencies locally
+    dependencies.Promise = require(script.Promise)
     dependencies.t = require(script.t)
 end
-
--- TEMPORARY PATCH: DONT USE ROBLOX-TS LIBRARY PROMISE (UNTIL REFACTOR IS OUT)
-dependencies.Promise = require(script.Promise)
 
 dependencies.HttpService = game:GetService("HttpService")
 
