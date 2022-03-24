ded905b9a5b8db512a902427fb72a932a156402d
David Duque <david@duqued.net>
2022-03-24 20:01:35 +0000

    Dependency loading via wally index

--- lib/DependencyLoader.lua
+++ lib/DependencyLoader.lua
@@ -7,7 +7,7 @@
 
 --[[
     EXTERNAL DEPENDENCIES:
-    - evaera/roblox-lua-promise (Built in the roblox-ts runtime)
+    - evaera/promise (Built in the roblox-ts runtime)
     - osyrisrblx/t (@rbxts/t)
 ]]
 
@@ -18,6 +18,10 @@ if TS then
     -- Module was loaded via Roblox-TS
     dependencies.Promise = TS.Promise
     dependencies.t = TS.import(script.Parent, TS.getModule(script.Parent, "t").lib.ts).t
+elseif script.Parent.Parent.Parent.Name == "_Index" then
+    -- This looks like a Wally package
+    dependencies.Promise = require(script.Parent.Parent.Promise)
+    dependencies.t = require(script.Parent.Parent.t)
 else
     -- Load dependencies locally
     dependencies.Promise = require(script.Promise)
